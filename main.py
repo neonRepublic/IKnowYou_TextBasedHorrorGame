0f4bcf1886cd89e523eba53d65e86ae7dde65a1c
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-08-04 12:07:07 -0400

    added folder install prompt and automation for folder creation on users desktop IKnowYou

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import time
 from datetime import datetime
 import os
 
-now = datetime.now()
-desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-install_folder = "InstallFolder"
+desktop_path = os.path.join(os.path.expanduser("~"), "OneDrive", "Desktop")
+install_folder = "IKnowYou"
 folder_path = os.path.join(desktop_path, install_folder)
 
 print("*It’s 3 AM. You’re the overnight IT guy. Your boss just sent you a link to install a new tool — something about querying company files faster.*")
@@ -16,7 +15,8 @@ user_name = input("Please enter your name below and press Enter:")
 print(f"\nThanks, {user_name}. Installing DocQuery Pro...")
 time.sleep(2)
 
-print("Current Date and Time: ", now) #Will change this later to add "time awareness" dialogue
+now = datetime.now()
+print("Current Date and Time: ", now)
 time.sleep(1)
 print(f"\nWow, would you look at the time {user_name}")
 time.sleep(1)
@@ -24,8 +24,19 @@ time.sleep(1)
 choice = input("\nDo you want to install folder on your pc? (yes/no): ").strip().lower()
 
 if choice == "yes":
-    print(f"Sounds good {user_name}, Now creating folder...")
+    print(f"Sounds good {user_name}, now creating folder...")
 elif choice == "no":
-    print(f"You have selected no. I'm dissapointed {user_name}. Installing anyway...")
+    print(f"You have selected no. I'm disappointed {user_name}. Installing anyway...")
 else:
     print("\nI didn't catch that, so auto installing...")
+
+print(f"Attempting to create folder at: {folder_path}")
+
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+    print("\nA new folder has appeared on your Desktop.")
+else:
+    print("\nThat folder is already on your Desktop.")
+
+time.sleep(2) 
