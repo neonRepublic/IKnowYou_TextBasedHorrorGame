64e55cbf8788d57c1b351586a83c043d12dc3122
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-08-04 11:15:29 -0400

    imported date/time library(s) and opening

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# Game Start prompt here
+import time
+from datetime import datetime
+
+now = datetime.now()
+
+print("*It’s 3 AM. You’re the overnight IT guy. Your boss just sent you a link to install a new tool — something about querying company files faster.*")
+time.sleep(2)
+print("\nWelcome to DocQuery Pro, please enter your name below.")
+time.sleep(1)
+user_name = input("Please enter your name below and press Enter:")
+
+print(f"\nThanks, {user_name}. Installing DocQuery Pro...")
+time.sleep(2)
+
+print("Current Date and Time: ", now) #Will change this later to add "time awareness" dialogue
