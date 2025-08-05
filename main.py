d4cd7b8541defbbf37b9e32e23c6411b969f0845
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-08-05 16:28:58 -0400

    updated code

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ elif choice == "no":
     print(f"\nYou have selected no. I'm disappointed {user_name}. Installing anyway...\n")
 else:
     print("\nI didn't catch that, so auto installing...\n")
-
-print(f"Attempting to create folder at...")
-
+time.sleep(2)
+print(f"Attempting to create folder...")
+time.sleep(2)
 
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
@@ -46,13 +46,14 @@ print("Under no circumstances should you delete this folder.")
 time.sleep(2)
 
 second_choice = input(f"Do you understand, {user_name}? (yes/no): ").strip().lower()
-
+time.sleep(1)
 if second_choice == "yes":
     print(f"\nI just knew you would understand, I think we'll get along just fine, {user_name}\n")
 elif second_choice == "no":
     print(f"\nI dare you to try...\n")
 else:
     print("\nThat was not an option but I'll assume you understand, {user_name}\n")
+time.sleep(2)
 
 print(f"\nNow, let's create a Username and Password: \n")
 time.sleep(1)
@@ -60,11 +61,13 @@ username = input("Username: ")
 password = getpass.getpass("Password: ")
 
 print("\nValidating credentials...\n")
-print(f"\nThat's a funny password {user_name}, I'll remember that...\n")
-print(f"\nPassword \"{password}\" committed to memory...\n")
-time.sleep(1)
-print("...you really thought I couldn't see that?")
 time.sleep(2)
+print(f"\nThat's a funny password {user_name}, I'll remember that...\n")
+time.sleep(2)
+print(f"\nPassword \"{password}\" committed to memory...\n")
+time.sleep(3)
+print("...you really thought I couldn't see that?")
+time.sleep(3)
 
 print("\n             *NEW EMAIL*         \n")
 time.sleep(2)
@@ -73,3 +76,6 @@ time.sleep(2)
 print("\n*It's from your boss...*\n")
 time.sleep(1)
 print(f"\n*It reads: URGENT: {user_name} don't install the software.*\n")
+time.sleep(2)
+
+print(f"\nIgnore that email, {user_name}...I mean it.\n")
